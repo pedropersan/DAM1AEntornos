7a3721c981f3304acf9dadc690bdeb1c65b4828d
pedro pérez <pedroperez@educa.jcyl.es>
2021-04-21 13:56:13 +0200

    validacion cambio2

--- PRUEBA11/src/cambio2/cambio2.java
+++ PRUEBA11/src/cambio2/cambio2.java
@@ -3,7 +3,10 @@ package cambio2;
 public class cambio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hola cambio1");
+		System.out.println("hola de nuevo enc cambio1");
+		System.out.println("hola mundo");
+		System.out.println("hola de nuevo enc mundo");
 
 	}
 
