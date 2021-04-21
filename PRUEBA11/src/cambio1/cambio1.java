18edf75e1cbe3ce2d45beedec3da69da2545909a
pedro pérez <pedroperez@educa.jcyl.es>
2021-04-21 13:46:24 +0200

    validacion de cambio1

---
+++ PRUEBA11/src/cambio1/cambio1.java
@@ -0,0 +1,11 @@
+package cambio1;
+
+public class cambio1 {
+
+	public static void main(String[] args) {
+		System.out.println("hola cambio1");
+		System.out.println("hola de nuevo enc cambio1");
+
+	}
+
+}
